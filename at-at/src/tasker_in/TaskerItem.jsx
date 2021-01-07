7bf671eb5d4b59e2ea60edd2382699f9bd98ea9f
Tom Thorpe <tom53086@gmail.com>
2021-01-06 20:01:28 -0800

    undo

--- at-at/src/tasker_in/TaskerItem.jsx
+++ at-at/src/tasker_in/TaskerItem.jsx
@@ -1,30 +1,30 @@
-// import React from "react";
-// import { DateTime } from 'luxon';
+import React from "react";
+import { DateTime } from 'luxon';
 
-// class TaskerItem extends React.Component {
-//   render() {
-//     const { 
-//       tasker_id, 
-//       priority_lvl, 
-//       suspense_date, 
-//       tasker_name, 
-//       predicted_workload 
-//     } = this.props.tasker;
+class TaskerItem extends React.Component {
+  render() {
+    const { 
+      tasker_id, 
+      priority_lvl, 
+      suspense_date, 
+      tasker_name, 
+      predicted_workload 
+    } = this.props.tasker;
     
-//     const localSuspense = DateTime.fromISO(suspense_date, {zone: 'utc'});
+    const localSuspense = DateTime.fromISO(suspense_date, {zone: 'utc'});
     
-//     return(
-//       <tr>
-//         <td><button type="button" id={tasker_id} onClick={this.props.showDetails}>View</button></td>
-//         <td>{tasker_id}</td>
-//         <td>{priority_lvl}</td>
-//         <td>{localSuspense.toLocaleString()}</td>
-//         <td>{tasker_name}</td>
-//         <td>{predicted_workload}</td>
-//       </tr>
-//     );
-//   }
-// }
+    return(
+      <tr>
+        <td><button type="button" id={tasker_id} onClick={this.props.showDetails}>View</button></td>
+        <td>{tasker_id}</td>
+        <td>{priority_lvl}</td>
+        <td>{localSuspense.toLocaleString()}</td>
+        <td>{tasker_name}</td>
+        <td>{predicted_workload}</td>
+      </tr>
+    );
+  }
+}
 
 
-// export default TaskerItem;
+export default TaskerItem;
