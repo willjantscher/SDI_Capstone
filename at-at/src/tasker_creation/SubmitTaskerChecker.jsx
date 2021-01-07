0db77809e1b295a701864e18b05a3691ed60566f
willjantscher <william.jantscher@gmail.com>
2021-01-07 08:19:11 -0700

    added checks for valid input and started user authentication integration

--- at-at/src/tasker_creation/SubmitTaskerChecker.jsx
+++ at-at/src/tasker_creation/SubmitTaskerChecker.jsx
@@ -5,11 +5,14 @@ import React from 'react'
 const  SubmitTaskerChecker = (tasker) => {
     // console.log(tasker)
     let flag = null;
+    // console.log( (new Date(tasker.suspense_date) - new Date(tasker.updated_on)))
 
     if(tasker.sendToUnits.length === 0) {
         flag = 'bad_sendToUnits'
     } else if(tasker.tasker_name === null) {
         flag = 'bad_tasker_name'
+    } else if(tasker.suspense_date === null || (new Date(tasker.suspense_date) - new Date(tasker.updated_on))< 1) {
+        flag = 'bad_suspense_date'
     } else if (tasker.predicted_workload === null) {
         flag = 'bad_predicted_workload'
     } else if (tasker.desc_text === null) {
@@ -21,8 +24,6 @@ const  SubmitTaskerChecker = (tasker) => {
     return(
         flag
     )
-
-
 
 }
 export default SubmitTaskerChecker;
