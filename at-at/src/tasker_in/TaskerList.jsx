0d2ad074902c34d54c161071af7f9071a4884d47
Alden Davidson <adavidson+github@protonmail.ch>
2021-01-08 13:14:00 -0800

    table can be sorted by clicking headers

--- at-at/src/tasker_in/TaskerList.jsx
+++ at-at/src/tasker_in/TaskerList.jsx
@@ -4,6 +4,75 @@ import TaskerItem from './TaskerItem';
 class TaskerList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      sortingMode: "predicted_workload",
+      sortDescending: false
+    };
+    this.headers = {
+      'ID': 'tasker_id',
+      'Priority': 'priority_lvl',
+      'Suspense': 'suspense_date',
+      'Name': 'tasker_name',
+      'Est. Workload': 'predicted_workload',
+    };
+    this.priorities = ['high', 'medium', 'low'];
+  }
+
+  generateColumnHeaders = () => {
+    return (
+      Object.keys(this.headers).map(headerName => {
+        return (
+          <th
+            key={headerName}
+            onClick={this.handleClickColumnHeader}
+          >
+          {headerName}
+          </th>
+        );
+      })
+    );
+  }
+
+  generateTaskerItems = (taskerArray) => {
+    taskerArray.sort((taskerA, taskerB) => {
+      const sortKey = this.state.sortingMode;
+      let a = taskerA[sortKey].toString().toLowerCase();
+      let b = taskerB[sortKey].toString().toLowerCase();
+      if (sortKey === this.headers['Priority']) {
+        return this.sortPriority(a, b);
+      }
+      if(a < b) {
+        return -1;
+      } else if (a > b) {
+        return 1;
+      }
+      return 0;
+    })
+
+    if (this.state.sortDescending) {
+      taskerArray.reverse();
+    }
+
+    const taskerItems = taskerArray.map(tasker => {
+      return (
+        <TaskerItem
+          key={`TaskerItem${tasker.tasker_id}`}
+          id={tasker.tasker_id}
+          tasker={tasker}
+          showDetails={this.props.showDetails}
+          onClick={this.props.onRowClick}
+          selected={this.isSelectedClassName(tasker)}
+        />
+      )
+    })
+
+    return taskerItems;
+  }
+
+  sortPriority = (a, b) => {
+    const aVal = this.priorities.indexOf(a);
+    const bVal = this.priorities.indexOf(b);
+    return aVal - bVal;
   }
 
   isSelectedClassName = (tasker) => {
@@ -12,31 +81,26 @@ class TaskerList extends React.Component {
     return isSelected ? "selected" : "";
   }
 
+  handleClickColumnHeader = (e) => {
+    const selectedHeader = e.target.textContent;
+    const sortKey = this.headers[selectedHeader];
+    let sortDescending = false;
+    if (sortKey === this.state.sortingMode) {
+      sortDescending = !this.state.sortDescending;
+    }
+    this.setState({sortingMode: sortKey, sortDescending: sortDescending});
+  }
+
   render() {
     return(
         <table className="rux-table">
           <thead>
             <tr>
-              <th>ID</th>
-              <th>Priority</th>
-              <th>Suspense</th>
-              <th>Name</th>
-              <th>Est. Workload</th>
+              {this.generateColumnHeaders()}
             </tr>
           </thead>
           <tbody>
-            {this.props.taskers.map(tasker => {
-              return (
-                <TaskerItem
-                  key={`TaskerItem${tasker.tasker_id}`}
-                  id={tasker.tasker_id}
-                  tasker={tasker}
-                  showDetails={this.props.showDetails}
-                  onClick={this.props.onRowClick}
-                  selected={this.isSelectedClassName(tasker)}
-                />
-              )
-            })}
+            {this.generateTaskerItems(this.props.taskers)}
           </tbody>
         </table>
     );
