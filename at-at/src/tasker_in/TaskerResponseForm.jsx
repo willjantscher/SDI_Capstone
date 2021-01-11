6a0a6614031d91edcea2c65bc0b26d9ab1411036
Alden Davidson <adavidson+github@protonmail.ch>
2021-01-11 08:24:58 -0800

    added formatting and response label

--- at-at/src/tasker_in/TaskerResponseForm.jsx
+++ at-at/src/tasker_in/TaskerResponseForm.jsx
@@ -4,22 +4,27 @@ class TaskerResponseForm extends React.Component {
   render() {
     return(
       <form className="container-fluid" onSubmit={this.props.onSubmit}>
-        <div className="row">
-          <textarea
-            className="mx-3"
-            name="taskerResponseData"
-            autoFocus={true}
-            defaultValue={this.props.defaultValue}
-          />
-        </div>
-        <div className="row">
-          <input
-            className="mx-3"
-            type="submit"
-            value="Submit"
-            name="taskerResponseSubmitButton"
-          />
-        </div>
+        <label
+          className="row mx-0"
+          for="taskerResponseData"
+        >
+          Response:
+        </label>
+        <textarea
+          className="row mx-0"
+          id="taskerResponseData"
+          name="taskerResponseData"
+          cols={100}
+          rows={10}
+          autoFocus={true}
+          defaultValue={this.props.defaultValue}
+        />
+        <input
+          className="row mx-0"
+          type="submit"
+          value="Submit"
+          name="taskerResponseSubmitButton"
+        />
       </form>
     );
   }
