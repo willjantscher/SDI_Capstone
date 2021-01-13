f76196cbd819182473bc5188cc2c6a596388a2d6
Alden Davidson <adavidson+github@protonmail.ch>
2021-01-13 14:01:01 -0800

    added workload field and updated formatting

--- at-at/src/tasker_in/TaskerResponseForm.jsx
+++ at-at/src/tasker_in/TaskerResponseForm.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 class TaskerResponseForm extends React.Component {
+  generateWorkloadOptions = () => {
+    let workloadOptions = [];
+    for(let i = 1; i < 25; i ++) {
+        workloadOptions.push(
+            <option id={i} key = {i} value={i}>{i}</option>
+        )
+    }
+    return workloadOptions;
+  }
+
   render() {
     const {
       desc_text,
@@ -15,16 +25,17 @@ class TaskerResponseForm extends React.Component {
         style={{display: display}}
       >
         <td colSpan="7">
-          <p className="row">{desc_text}</p>
+          <p className="row mt-1">{desc_text}</p>
+          <hr/>
           <form onSubmit={this.props.onSubmitResponse}>
             <label
-              className="row"
+              className="row mt-3"
               htmlFor="taskerResponseData"
             >
               Response:
             </label>
             <textarea
-              className="row rux-form-field--large will-colors"
+              className="row mt-1 rux-form-field--large will-colors"
               id="taskerResponseData"
               name="taskerResponseData"
               cols={100}
@@ -32,8 +43,24 @@ class TaskerResponseForm extends React.Component {
               autoFocus={true}
               defaultValue={currentResponse ? currentResponse : ""}
             />
+            <div className="row mt-2">
+              <label
+                className="mr-2 mt-1"
+                htmlFor="taskerResponseWorkload"
+              >
+                Workload to define response:
+              </label>
+              <select
+                className="rux-select col-md-1 will-colors"
+                id="actual_workload"
+                placeholder="hrs"
+              >
+                <optgroup label="Hours"></optgroup>
+                {this.generateWorkloadOptions()}
+              </select>
+            </div>
             <input
-              className="will-colors rux-button row"
+              className="will-colors rux-button row mt-2"
               type="submit"
               value="Submit"
               name="taskerResponseSubmitButton"
